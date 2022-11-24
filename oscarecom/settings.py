8446a05b5b9cd4f2289c0bdd9aa72c78a06aa059
sreesankar12 <sreesankar@sayonetech.com>
2022-11-24 15:55:11 +0530

    commit message

--- oscarecom/settings.py
+++ oscarecom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6i@(s!5a!i8f15hb%+smioq=k40(y^g^8zmeincu*szmrnf)wb
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
